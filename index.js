f62a0ca2c4d75fd8937bac6d58e84aed4319bb74
oncelseker <oncelseker@gmail.com>
2025-03-16 20:44:29 +0300

    solution of index js

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ function KareninAlani(kenaruzunlugu) {
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
-function CemberinCevresi(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinCevresi(yaricap) {
+  return 2 * pi * yaricap;
 }
 
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -64,8 +64,8 @@ function CemberinCevresi(/* kodlar buraya */) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yaricap) {
+  return pi * Math.pow(yaricap, 2);
 }
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
@@ -99,27 +99,52 @@ let ucetambolunenler,
 
 // 3a çözümü
 
-/* kodlar buraya */
+enbuyuk = sayilar[0];
+enkucuk = sayilar[0]; 
+
+for (let i = 0; i < sayilar.length; i++) {
+  if (sayilar[i] > enbuyuk) {
+    enbuyuk = sayilar[i];
+  }
+  if (sayilar[i] < enkucuk) {
+    enkucuk = sayilar[i];
+  }
+}
 
 // 3b çözümü:
 
-/* kodlar buraya */
+ucetambolunenler = sayilar.filter((sayi) => sayi % 3 === 0);
 
 // 3c çözümü:
 
-/* kodlar buraya */
+ucebolunenlerintoplami = ucetambolunenler.reduce((toplam, sayi) => toplam + sayi, 0);
 
 // 3d çözümü
 
-/* kodlar buraya */
+besyuzdenkucuksayilar = sayilar.filter((sayi) => sayi < 500);
 
 // 3e çözümü
 
-/* kodlar buraya */
+siralisayilar = besyuzdenkucuksayilar.sort((a, b) => a - b);
 
 // 3f çözümü
 
-/* kodlar buraya */
+let tekrarEdenSayilarObje = {};
+sayilar.forEach((sayi) => {
+  if (tekrarEdenSayilarObje[sayi]) {
+    tekrarEdenSayilarObje[sayi]++;
+  } else {
+    tekrarEdenSayilarObje[sayi] = 1;
+  }
+}); 
+
+tekraredensayilar = [];
+for (let sayi in tekrarEdenSayilarObje) {
+  if (tekrarEdenSayilarObje[sayi] > 1) {
+    tekraredensayilar.push(`${sayi} sayisi ${tekrarEdenSayilarObje[sayi]} kere tekrar edilmiştir`);
+  }
+} 
+
 
 /*  Bu satırın aşağısındaki kodları lütfen değiştirmeyin  */
 
